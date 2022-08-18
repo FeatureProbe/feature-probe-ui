07f22d60d71627dad244e4d2068e422919ea1309
frankzhaopeng <poryzhao@featureprobe.com>
2022-08-18 14:18:17 +0800

    feat: add toast

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import dayjs from 'dayjs';
+import { ToastContainer } from 'react-toastify';
 import Router from './router';
 import { I18NContainer } from './hooks';
 import relativeTime from 'dayjs/plugin/relativeTime';
@@ -9,6 +10,7 @@ import Intl from './locales/intl';
 import 'iconfont/iconfont.css';
 import 'semantic-ui-less/semantic.less';
 import "react-datetime/css/react-datetime.css";
+import 'react-toastify/dist/ReactToastify.css';
 import 'dayjs/locale/zh-cn';
 
 import './index.scss';
@@ -22,6 +24,7 @@ ReactDOM.render(
       <Intl>
         <Router />
       </Intl>
+      <ToastContainer theme='colored' />
     </I18NContainer.Provider>
   </React.StrictMode>,
   document.getElementById('root')
