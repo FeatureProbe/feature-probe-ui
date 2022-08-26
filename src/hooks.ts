cb3be448b5f1e634c935e1ea6dba45ff8b2e0e82
panchongyang <yourhand1025@163.com>
2022-08-26 08:03:02 +0800

    * Fixed autofill
    Fixed a bug：
    The new feature: drop the response of old requests,
    it is invalid and makes debounce is incorrect
    
    Signed-off-by: Pan Chongyang <panchongyang.p@qq.com>

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,3 +1,4 @@
+import { useCallback, useRef } from "react";
 import { createContainer } from "unstated-next";
 import { useLocalStorage } from "utils/hooks";
 
@@ -13,14 +14,20 @@ export const useI18N = () => {
 export const I18NContainer = createContainer(useI18N);
 
 export const useSearchTime = () => {
-  let searchTime: number = 0;
+  let searchTime = useRef(0);
+
+  const saveSearchTime = (time: number) => {
+    searchTime.current = time;
+  }
+
+  const setSearchTime = useCallback(saveSearchTime, []);
+
+  const check = useCallback((time: number) => {
+    return time === searchTime.current;
+  }, [searchTime]);
+
   return {
-    searchTime,
-    setSearchTime: (time: number) => {
-      searchTime = time;
-    },
-    check: (time: number) => {
-      return time === searchTime;
-    }
+    setSearchTime,
+    check
   }
 }
