3c29a955ce340fcca0c1165281267adce3cd259f
frankzhaopeng <poryzhao@featureprobe.com>
2022-10-24 15:56:47 +0800

    fix: issue problem

--- src/pages/approval/components/Lists/index.tsx
+++ src/pages/approval/components/Lists/index.tsx
@@ -51,10 +51,10 @@ const Lists = () => {
 				const { content, pageable, totalPages, totalElements } = data;
 				saveApprovalList(content);
 				setPagination({
-					pageIndex: pageable.pageNumber + 1,
-					totalPages,
+					pageIndex: (pageable?.pageNumber || 0) + 1,
+          totalPages: totalPages || 1,
 				});
-				setTotal(totalElements);
+				setTotal(totalElements || 0);
 			}
       else {
         message.error(intl.formatMessage({id: 'approvals.lists.error'}));
